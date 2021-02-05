fdcb8f630db888ae363b54a11b9c9342c7c12938
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-02-05 16:43:34 +0530

    added another export

--- switchToGraal.sh
+++ switchToGraal.sh
@@ -1,4 +1,5 @@
 export PATH=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0/Contents/Home/bin:$PATH
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0/Contents/Home
+export PATH=/Users/vijaykumarab/Google\ Drive/GraalVM-Book/Tools/idealgraphvisualizer/bin/:$PATH
 
 xattr -d com.apple.quarantine /Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0/
