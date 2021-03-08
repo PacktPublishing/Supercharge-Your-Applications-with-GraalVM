ada574df649cd5ce52a9e9375aab33efa3c3f009
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-03-08 23:53:32 +0530

    Comit

--- chapter4/FibonacciCalculator.java
+++ chapter4/FibonacciCalculator.java
@@ -1,40 +1,47 @@
+//package com.packt.ebook.graalvm.chapter4;
+
 class FibonacciCalculator{  
 
     public int[] findFibonacci(int count) {
         int fib1 = 0;
         int fib2 = 1;
+
         int currentFib, index;
+
+        
         int [] fibNumbersArray = new int[count];
           
         for(index=2; index < count; ++index ) {    
+            
             currentFib = fib1 + fib2;    
+            
             fib1 = fib2;    
             fib2 = currentFib;    
+
             fibNumbersArray[index - 1] = currentFib;
+
         }    
+
         return fibNumbersArray;
+   
     }
 
     public static void main(String args[])  
     {    
         FibonacciCalculator fibCal = new FibonacciCalculator();
-        long startTime = System.currentTimeMillis();
-        long now = 0;
-        long last = startTime;
-        for (int i = 1000000000; i < 1000000010; i++) {
-            int[] fibs = fibCal.findFibonacci(i);
-            
-            long total = 0;
-            for (int j=0; j<fibs.length; j++) {
-                total += fibs[j];
-            }
+        
+        
 
-            now = System.currentTimeMillis();
-            System.out.printf("%d (%d ms)%n", i , now - last);
-            last = now;
+        for (int i = 1000000; i < 1000000000; i++) {
+            long startTime = System.currentTimeMillis();
+            fibCal.findFibonacci(i);
+
+            long endTime = System.currentTimeMillis();
+            long timeLapse = (endTime - startTime)/1000;
+            
+            System.out.println(" Sequence for " + i + " Fibonacci numbers in " + timeLapse);    
+            
         }
-        long endTime = System.currentTimeMillis();
-        System.out.printf("total: (%d ms)%n", System.currentTimeMillis() - startTime);  
       
     }
 }
