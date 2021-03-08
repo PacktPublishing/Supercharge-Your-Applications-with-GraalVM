1b7d5661f9a45f43a957d105f17aa98c74865e3b
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-03-08 23:53:32 +0530

    Comit

--- switchToGraal.sh
+++ switchToGraal.sh
@@ -1,20 +1,5 @@
-export PATH=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0.2/Contents/Home:$PATH
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0.2/Contents/Home 
-export PATH=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0.2/Contents/Home/tools/idealgraphvisualizer/bin
-
-export PATH=/Library/Java/JavaVirtualMachines/graalvm-ee-java8-21.0.0.2/Contents/Home:$PATH
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ee-java8-21.0.0.2/Contents/Home
-
-export PATH=/Library/Java/JavaVirtualMachines/graalvm-ce-java8-21.0.0.2/Contents/Home:$PATH
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java8-21.0.0.2/Contents/Home
-
-export PATH=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-21.0.0.2/Contents/Home:$PATH
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java8-21.0.0.2/Contents/Home
-
-/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0.2/Contents/Home/tools/idealgraphvisualizer/bin/idealgraphvisualizer --jdkhome /Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0.2/Contents/Home
-
-
-
-
+export PATH=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0/Contents/Home/bin:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0/Contents/Home
+export PATH=/Users/vijaykumarab/Google\ Drive/GraalVM-Book/Tools/idealgraphvisualizer/bin/:$PATH
 
 xattr -d com.apple.quarantine /Library/Java/JavaVirtualMachines/graalvm-ee-java11-21.0.0/
