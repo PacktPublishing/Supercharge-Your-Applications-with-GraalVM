3d05d54f7d4f8c068ba374fe0e5449ed346ac4d5
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-03-15 10:25:58 +0530

    Updates

--- chapter 6/node.java
+++ chapter 6/node.java
@@ -16,3 +16,39 @@ abstract class Node {
     File path = new File("/path/to/scriptfile");
     Source pythonScript = Source.newBuilder("python", new File(path, "pythonScript.py")).build();
     ctx.eval(pythonScript)
+
+
+
+
+
+    @Specialization protected long executeAddInt (int left, int right) {
+        return left + right;
+    }
+    @Specialization String executeAddFloat (Float left, Float right) {
+        return left + right;
+    }
+    @Specialization String executeAddString (String left, String right) {
+        return left + right;
+    }
+    @Fallback protected void typeError (Object left, Object right) {
+        throw new TypeException("type error: args must be two integers or floats or two", this);
+    }
+
+
+    @Specialization(guards = {"!isInteger(operand)", "!isFloat(operand)"})
+    protected final int executeTheMethod(final Object operand) {
+      //....code to execute if the expression is true
+    }
+
+
+
+@Specialization(rewriteOn = ArithmeticException.class)
+ int executeNoOverflow(int a, int b) {
+     return Math.addExact(a, b);
+ }
+ @Specialization
+ long executeWithOverflow(int a, int b) {
+     return a + b;
+ }
+
+ 
