d98f1165cd62dcf5d0fc2554bdcfadbbdcea9db0
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-03-16 19:58:16 +0530

    Python

--- chapter8/python/math.js
+++ chapter8/python/math.js
@@ -1,14 +1,31 @@
 
-var helloMathMessage = " Hello Math.js Variable";
+var helloMathMessage = " Hello Math.js";
 
 function add(a, b) {
+    print("message from js: add() called");
     return a+b;
 }
 
 function subtract(a, b) {
+    print("message from js: subtract() called");
     return a-b;
 }
 
 function multiply(a, b) {
+    print("message from js: multiply() called");
     return a*b;
 }
+
+function square(a) {
+    print("message from js: square() called");
+    Polyglot.evalFile('python', './mathUtil.py');
+    pySquare = Polyglot.import('square');
+    result = pySquare(a)
+    return result
+}
+
+Polyglot.export('add', add);
+Polyglot.export('subtract', subtract);
+Polyglot.export('multiply', multiply);
+Polyglot.export('square', square);
+Polyglot.export('message', helloMathMessage)
