7c8aebdb727b2ca61cce4330f84c851a10acf725
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-03-20 11:01:55 +0530

    main

--- chapter8/espresso/EspressoPolyglot.java
+++ chapter8/espresso/EspressoPolyglot.java
@@ -1,11 +1,11 @@
-
 import com.oracle.truffle.espresso.polyglot.Polyglot;
-
 public class EspressoPolyglot {
     public static void main(String[] args) {
-
-        Object mathjs = Polyglot.eval("js", "print('Hello Espresso Polyglot');");
-        
+        try {
+            Polyglot.eval("js", "print('Hello from JS on Espresso');");
+            Object mathjs = Polyglot.eval("js", "print('Hello there from JS on Espresso');");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
 }
