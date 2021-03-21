e6e598dd47af7bb5220fcd330dff430e944a37dd
A B Vijay Kumar <6584323+abvijaykumar@users.noreply.github.com>
2021-03-21 20:05:07 +0530

    Added files for Wasm and R

---
+++ chapter8/wasm/FibonacciWasmCaller.java
@@ -0,0 +1,30 @@
+import java.io.File;
+import java.nio.file.Files;
+
+import org.graalvm.polyglot.*;
+import org.graalvm.polyglot.io.ByteSequence;
+
+public class FibonacciWasmCaller {
+    public static void main(String[] args) {
+        try {
+            File file = new File("fibonacci.wasm");
+            byte[] wasmBinaryArray = Files.readAllBytes(file.toPath());
+            Context.Builder contextBuilder = Context.newBuilder("wasm");
+            Source.Builder sourceBuilder = Source.newBuilder("wasm", ByteSequence.create(wasmBinaryArray), "fibonacci");
+            Source source = sourceBuilder.build();
+            Context context = contextBuilder.build();
+
+            context.eval(source);
+
+            Value bindings = context.getBindings("wasm");
+            System.out.println((bindings.getMemberKeys()));
+
+            Value main = bindings.getMember("main");
+            System.out.println(main.hasMembers());
+            System.out.println(main.getMemberKeys());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
